33ef28f7dd7fc9602bd8ff634240633a2aeb4c1a
Fran Zekan <zekan.fran369@gmail.com>
2023-01-04 14:53:44 +0100

    cpp format

--- day-03/main.cpp
+++ day-03/main.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <vector>
 
-bool is_triangle(int a, int b, int c)
-{
-  return a + b > c && a + c > b && b + c > a;
-}
+bool is_triangle(int a, int b, int c) { return a + b > c && a + c > b && b + c > a; }
 
-int main()
-{
-  int x1 = 0;
-  int x2 = 0;
+int main() {
+    int x1 = 0;
+    int x2 = 0;
 
-  int a, b, c;
-  std::vector<std::vector<int>> triangles;
+    int a, b, c;
+    std::vector<std::vector<int>> triangles;
 
-  while (std::cin >> a >> b >> c)
-  {
-    triangles.emplace_back(std::vector<int>{a, b, c});
-    if (is_triangle(a, b, c))
-    {
-      ++x1;
+    while (std::cin >> a >> b >> c) {
+        triangles.emplace_back(std::vector<int>{a, b, c});
+        if (is_triangle(a, b, c)) {
+            ++x1;
+        }
     }
-  }
 
-  for (int row = 0; row < triangles.size(); row += 3)
-  {
-    for (int col = 0; col < 3; ++col)
-    {
-      if (is_triangle(triangles[row][col], triangles[row + 1][col], triangles[row + 2][col]))
-      {
-        ++x2;
-      }
+    for (int row = 0; row < triangles.size(); row += 3) {
+        for (int col = 0; col < 3; ++col) {
+            if (is_triangle(triangles[row][col], triangles[row + 1][col],
+                            triangles[row + 2][col])) {
+                ++x2;
+            }
+        }
     }
-  }
 
-  std::cout << x1 << std::endl;
-  std::cout << x2 << std::endl;
+    std::cout << x1 << std::endl;
+    std::cout << x2 << std::endl;
 
-  return 0;
+    return 0;
 }
