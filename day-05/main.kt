6d23c2a08bc7ec271db4ce60e46787b6a0727e36
Fran Zekan <zekan.fran369@gmail.com>
2023-01-06 00:33:06 +0100

    Format day5

--- day-05/main.kt
+++ day-05/main.kt
@@ -1,18 +1,18 @@
 import java.math.BigInteger
 import java.security.MessageDigest
 
-fun md5(input:String): String {
-    val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+fun md5(input: String): String {
+  val md = MessageDigest.getInstance("MD5")
+  return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
 }
 
 fun main() {
   val password1 = StringBuilder()
   val password2 = CharArray(8) { ' ' }
 
-  for(i in 1..1000000000) {
+  for (i in 1..1000000000) {
     val hash = md5("reyedfim$i")
-    if(hash.startsWith("00000")) {
+    if (hash.startsWith("00000")) {
       if (password1.length < 8) {
         password1.append(hash[5])
       }
